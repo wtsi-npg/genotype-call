adb5f7a39278c6b6c4045b027de2ae3405450f33
Keith James <kdj@sanger.ac.uk>
2012-07-13 08:22:04 +0100

    Changed JSON export of SNP metadata to use event-driven writing.
    Reduced dynamic space size request for building.
    
    Disabled saving the dynamic space size in the image (enables setting by CLI).

--- src/bpm.lisp
+++ src/bpm.lisp
@@ -165,19 +165,19 @@ with string values and key \"position\" with an integer value.")
    (:method (filespec (manifest bpm) &key key test)
      (with-open-file (out filespec :direction :output :if-exists :supersede
                           :if-does-not-exist :create)
-       (loop
-          with snps = (snps-of manifest :key key :test test)
-          for snp across snps
-          collect (pairlis '(:name :chromosome :position
-                             :allele_a :allele_b :norm-id)
-                           (list (snp-name snp)
-                                 (encode-bim-chromosome (snp-chromosome snp))
-                                 (snp-position snp)
-                                 (snp-allele-a snp) (snp-allele-b snp)
-                                 (snp-norm-id snp))) into specs
-          finally (with-underscore-translation
-                    (json:encode-json specs out)
-                    (return specs))))))
+       (with-underscore-translation
+         (json:with-array (out)
+           (loop
+              for snp across (snps-of manifest :key key :test test)
+              do (json:as-array-member (out)
+                   (json:encode-json
+                    (pairlis '(:name :chromosome :position
+                               :allele_a :allele_b :norm-id)
+                             (list (snp-name snp)
+                                   (encode-bim-chromosome (snp-chromosome snp))
+                                   (snp-position snp)
+                                   (snp-allele-a snp) (snp-allele-b snp)
+                                   (snp-norm-id snp))) out))))))))
 
 (defun cnv-probe-p (probe-name)
   "Returns T if the PROBE-NAME indicates a copy-number variation (CNV)
